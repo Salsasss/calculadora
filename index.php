51405d33fcaee3431ca481483ef005359bbf5a23
Aaron Salas <aaronsalasn@gmail.com>
2022-07-07 17:09:52 -0500

    Add index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+</head>
+
+<body>
+    <div class="calculadora">
+        <div class="resultado">2x2</div>
+        <div class="botones">
+            <a class="boton"></a>
+            <a class="boton"></a>
+            <a class="boton"></a>
+            <a class="boton">/</a>
+            <a class="boton">7</a>
+            <a class="boton">8</a>
+            <a class="boton">9</a>
+            <a class="boton">x</a>
+            <a class="boton">4</a>
+            <a class="boton">5</a>
+            <a class="boton">6</a>
+            <a class="boton">-</a>
+            <a class="boton">1</a>
+            <a class="boton">2</a>
+            <a class="boton">3</a>
+            <a class="boton">+</a>
+            <a class="boton">.</a>
+            <a class="boton">0</a>
+            <a class="boton res">=</a>
+        </div>
+    </div>
+    <script src="js.js"></script>
+</body>
+
+</html>
